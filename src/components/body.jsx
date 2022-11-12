cb807d49a746a891af70b3e3e08023692da4567c
JamesHiro <unicornjameser@gmail.com>
2022-11-12 16:22:01 +0900

    Update dropdown select

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,11 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 const Body = () => {
-  const money = 0.1287; 
-  const [coinmenu, setCoinMenu] = useState(false);
-  const [datemenu, setDateMenu] = useState(false);
+  const money = 0.1287;
   const [selectedCoin, setCoinSelect] = useState(0);
   const [selectedDate, setDateSelect] = useState(0);
+  const coinref = useRef();
+  const dateref = useRef();
+  const [isCoinMenuOpen, setIsCoinMenuOpen] = useState(false);
+  const [isDateMenuOpen, setIsDateMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const checkIfClickedOutside = e => {
+      if (isCoinMenuOpen && coinref.current && !coinref.current.contains(e.target)) {
+        setIsCoinMenuOpen(false)
+      }
+    }
+
+    document.addEventListener("mousedown", checkIfClickedOutside)
+
+    return () => {
+      document.removeEventListener("mousedown", checkIfClickedOutside)
+    }
+  }, [isCoinMenuOpen])
+
+  useEffect(() => {
+    const checkIfClickedOutside = e => {
+      if (isDateMenuOpen && dateref.current && !dateref.current.contains(e.target)) {
+        setIsDateMenuOpen(false)
+      }
+    }
+
+    document.addEventListener("mousedown", checkIfClickedOutside)
+
+    return () => {
+      document.removeEventListener("mousedown", checkIfClickedOutside)
+    }
+  }, [isDateMenuOpen])
 
   const crypto_prices = [
     {
@@ -70,42 +100,35 @@ const Body = () => {
     },
   ];
 
-  const icons = [
-    "/icon/btc.png",
-    "/icon/eth.png",
-    "/icon/usdc.png",
-    "/icon/usdt.png",
-    "/icon/bnb.png",
-  ];
-
   const dateranges = [
-    {name:"PL1D", date:1, text:"1 day"}, 
-    {name:"PL7D", date:7, text:"7 days"}, 
-    {name:"PL30D", date:30, text:"30 days"}
+    { name: "PL1D", date: 1, text: "1 day" },
+    { name: "PL7D", date: 7, text: "7 days" },
+    { name: "PL30D", date: 30, text: "30 days" },
   ];
 
   const handleChangeDataRange = (index) => {
     setDateSelect(index);
-    setDateMenu(!datemenu);
+    setIsDateMenuOpen(!isDateMenuOpen);
   };
 
   const handleCoinSelected = (index) => {
     setCoinSelect(index);
-    setCoinMenu(!coinmenu);
+    setIsCoinMenuOpen(!isCoinMenuOpen);
   }
 
-  const range = dateranges[selectedDate].name;
+  console.log(isCoinMenuOpen)
 
+  const range = dateranges[selectedDate].name;
   const pvlpercent = parseFloat(crypto_prices[selectedCoin][range]).toFixed(2);
   const pvlvalue = parseFloat(
     money * crypto_prices[selectedCoin][range] * crypto_prices[selectedCoin].price,
   ).toFixed(2);
 
   return (
-    <div>
-      <div className="md:flex justify-center items-center md:px-20 px-5 py-28">
+    <div className="text-white">
+      <div className="md:flex justify-center lg:px-20 md:px-10 px-5 py-28">
         <div className="text-center lg:mr-20 mr-5 mb-20 md:max-w-[600px]">
-          <p className="text-6xl text-white font-semibold mb-10">
+          <p className="lg:text-6xl md:text-5xl text-4xl font-semibold mb-10">
             Manage your crypto portfolio risk
           </p>
           <p className="text-[#ddd] mb-10">
@@ -113,51 +136,52 @@ const Body = () => {
             crypto market. One Cigma is here to help you with that.
           </p>
           <div className="flex justify-center">
-            <button className="border border-black rounded-lg bg-[#000000] text-white py-2 px-4">
+            <button className="border border-black rounded-lg bg-[#000000] py-2 px-4">
               Manage risk
             </button>
           </div>
         </div>
 
-        <div className="border rounded-lg bg-[#ffffff00] lg:p-10 p-5 md:max-w-[500px] md:min-w-[320px]">
+        <div className="border rounded-lg bg-[#ffffff00] lg:p-10 p-5 min-w-[320px] lg:min-w-[400px] lg:max-w-[500px]">
           <p className="md:block flex justify-center mb-8 text-[#ddd] text-xl">
             Calculate the potential risk you are taking
           </p>
-          <p className="w-4/12 text-center text-white">You have</p>
-          <div className="mb-8 flex">
-            <div className="relative text-center text border border-white-center text-white rounded-lg py-3 mr-2 w-4/12">
+          <p className="text-center w-4/12">You have</p>
+          <div className="mb-8 flex justify-between">
+            <div className="relative text-center text border border-white-center rounded-lg py-3 mr-1 w-4/12">
               <p className="text-[#ddd] font-semibold py-1">{money}</p>
             </div>
 
-            <div className="relative content-center text border border-white-center text-white rounded-lg py-3 mr-2 w-4/12">
-              <button className={`flex items-center py-1 px-2 text-white w-full`} onClick={(e) => setCoinMenu(!coinmenu)}>
-                <img src={icons[selectedCoin]} width={20} height={20} alt="" />
+            <div className="relative content-center text border border-white-center rounded-lg py-3 mr-1 w-4/12">
+              <button className={`flex items-center py-1 px-2 w-full`} onClick={() => setIsCoinMenuOpen(!isCoinMenuOpen)}>
+                <img src={crypto_prices[selectedCoin].icon} width={20} height={20} alt="" />
                 <p className="w-full text-center">{crypto_prices[selectedCoin].symbol}</p>
-                <img src="/icon/down.svg" className={`${!coinmenu ? "block" : "hidden"} mr-2`} width={10} height={10} alt="" />
-                <img src="/icon/up.svg" className={`${coinmenu ? "block" : "hidden"} mr-2`} width={10} height={10} alt="" />
+                <img src="/icon/down.svg" className={`${!isCoinMenuOpen ? "block" : "hidden"} mr-2`} width={10} height={10} alt="" />
+                <img src="/icon/up.svg" className={`${isCoinMenuOpen ? "block" : "hidden"} mr-2`} width={10} height={10} alt="" />
               </button>
-              <div className={`${coinmenu ? "block" : "hidden"} absolute rounded-b-lg bg-[#00000080] text-white`}>
+              {isCoinMenuOpen && (
+              <div className={`${isCoinMenuOpen ? "block" : "hidden"} absolute rounded-b-lg bg-[#00000080] w-full mt-3`} ref={coinref}>
                 {crypto_prices.map((coin, index) => (
                   <div>
                     <div className="w-full border border-white border-1" />
-                    <button className="flex  p-2" onClick={() => handleCoinSelected(index)}>
-                      <img src={icons[index]} className="mr-2" width={20} height={20} alt="" />
+                    <button className="flex w-full p-2" onClick={() => handleCoinSelected(index)}>
+                      <img src={crypto_prices[index].icon} className="mr-2" width={20} height={20} alt="" />
                       {coin.symbol}
                     </button>
                   </div>
                 ))}
-              </div>
+              </div>)}
             </div>
-            
-            <div className="relative content-center border border-white text-center text-white rounded-lg py-3 w-4/12">
-              <button className="flex items-center p-1 w-full" onClick={(e) => setDateMenu(!datemenu)}>
+
+            <div className="relative content-center border border-white text-center rounded-lg py-3 w-4/12">
+              <button className="flex items-center p-1 w-full" onClick={(e) => setIsDateMenuOpen(!isDateMenuOpen)} >
                 <div className="w-full justify-center flex">
                   {dateranges[selectedDate].text}
                 </div>
-                <img src="/icon/down.svg" className={`${!datemenu ? "block" : "hidden"} mr-2`} width={10} height={10} alt="" />
-                <img src="/icon/up.svg" className={`${datemenu ? "block" : "hidden"} mr-2`} width={10} height={10} alt="" />
+                <img src="/icon/down.svg" className={`${!isDateMenuOpen ? "block" : "hidden"} mr-2`} width={10} height={10} alt="" />
+                <img src="/icon/up.svg" className={`${isDateMenuOpen ? "block" : "hidden"} mr-2`} width={10} height={10} alt="" />
               </button>
-              <div className={`${datemenu ? "block" : "hidden"} absolute rounded-b-lg bg-[#00000080] text-left`}>
+              <div className={`${isDateMenuOpen ? "block" : "hidden"} absolute rounded-b-lg bg-[#00000080] text-left w-full mt-3`} ref={dateref}>
                 {dateranges.map((daterange, index) => (
                   <div>
                     <div className="w-full border border-white border-1" />
@@ -169,33 +193,28 @@ const Body = () => {
               </div>
             </div>
           </div>
-          <div>
-            <p className="mb-10 text-center text-[#ddd] text-[30px] font-bold">
+          <div className="lg:text-[30px] text-[24px] font-bold">
+            <p className="mb-10 text-center text-[#ddd]">
               The potential loss of{" "}
-              <le className="crypto_name text-[30px] font-bold">
+              <le className="crypto_name">
                 {crypto_prices[selectedCoin].name}
               </le>{" "}
               over{" "}
-              <le className="text-[#ffd100] text-[30px] font-bold">
+              <le className="text-[#ffd100]">
                 {dateranges[selectedDate].text}
               </le>{" "}
-              is{" "}
-              <le className="text-[#ffd100] text-[30px] font-bold">
-                {pvlpercent}%
-              </le>{" "}
+              is <le className="text-[#ffd100]">{pvlpercent}%</le>{" "}
             </p>
-            <p className="text-center text-[#ddd] text-[30px] font-bold">
+            <p className="text-center text-[#ddd]">
               Your risk is{" "}
-              <le className="text-[#ffd100] text-[30px] font-bold">
-                ${pvlvalue}
-              </le>
+              <le className="text-[#ffd100] font-bold">${pvlvalue}</le>
             </p>
           </div>
         </div>
       </div>
 
       <div className="md:flex md:px-20 px-5 mt-28 overflow-x-auto">
-        <table className="risk_table text-white w-full space-between">
+        <table className="risk_table w-full space-between">
           <thead>
             <th className="min-w-[20px]">#</th>
             <th className="min-w-[180px]">Name</th>
@@ -214,14 +233,8 @@ const Body = () => {
                 <tr className="text-[#ddd] h-8 items-center">
                   <td>{index + 1}</td>
                   <td className="flex pt-1">
-                    <img
-                      src={icons[index]}
-                      width={24}
-                      height={20}
-                      className="mr-2"
-                      alt=""
-                    />{" "}
-                    {crypto.name} {crypto.symbol}
+                    <img src={crypto_prices[index].icon} width={24} height={20} className="mr-2" alt="" />{" "}
+                    {crypto.name} ({crypto.symbol})
                   </td>
                   <td>{crypto.price}</td>
                   <td>12</td>
@@ -239,7 +252,7 @@ const Body = () => {
       </div>
 
       <div className="flex md:px-20 px-5 mt-20 justify-center">
-        <button className="text-white font-semibold flex items-center">
+        <button className="font-semibold flex items-center">
           See the full list of cryptocurrencies
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -259,7 +272,7 @@ const Body = () => {
 
       <div className="md:flex justify-center md:px-20 px-5 mt-48 pb-20">
         <div className="lg:mr-20 mr-5 mb-20 md:max-w-[500px] md:w-6/12 ">
-          <p className="text-center text-4xl text-white font-semibold mb-10">
+          <p className="text-center text-4xl font-semibold mb-10">
             Before buying a risky coin, check One Cigma
           </p>
           <p className="text-center text-[#ddd] mb-10">
@@ -268,20 +281,10 @@ const Body = () => {
             Potential Value Loss before buying any coin.
           </p>
           <div className="flex justify-center">
-            <button className="text-white font-semibold flex items-center">
+            <button className="font-semibold flex items-center">
               See the full list of cryptocurrencies
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-arrow-right ml-2"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
-                />
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right ml-2" viewBox="0 0 16 16">
+                <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
               </svg>
             </button>
           </div>
